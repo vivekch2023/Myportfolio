19e4acb259437fa2c53345a1b71e218c99ef2d18
vivekch2023 <166383849+vivekch2023@users.noreply.github.com>
2026-02-12 22:00:14 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
@@ -10,18 +10,20 @@ import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
-    <>
-      <div>
-        <Navbar />
-        <Home />
-        <About />
-        <Skill />
-        <PortFolio />
-        <Contact />
-        <Footer />
-      </div>
+    <Router>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/skills" element={<Skill />} />
+        <Route path="/portfolio" element={<PortFolio />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+
+      <Footer />
       <Toaster />
-    </>
+    </Router>
   );
 }
 
